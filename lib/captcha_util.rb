95b220544431cff50598492698fd7be2f37c6520
Bernat Foj Capell <bfoj@gnuine.com>
2011-08-13 13:12:06 +0200

    Raise exception if no captcha images are found, instead of failing obscurently

--- lib/captcha_util.rb
+++ lib/captcha_util.rb
@@ -4,6 +4,7 @@ module CaptchaUtil
 
   def self.random_image
     @@captcha_files ||= Dir.glob("#{Rails.root}/public/system/captcha/*.*").map {|f| File.basename(f)}
+    raise 'No captcha images found. Run rake captcha:generate to generate them' if @@captcha_files.blank?
     @@captcha_files[rand(@@captcha_files.size)]
   end
 
@@ -13,7 +14,7 @@ module CaptchaUtil
     if defined?(CAPTCHA_SALT)
       salt = CAPTCHA_SALT
     else
-      Rails.logger.warn("No salt defined, please add CAPTHCA_SALT = 'Something really random' to environment.rb")
+      Rails.logger.warn("No salt defined, please add CAPTCHA_SALT = 'Something really random' to environment.rb")
     end  
 
     Digest::SHA1.hexdigest("#{salt}#{string}")
